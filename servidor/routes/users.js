f1667d63e718f5dfc38f77110701ba283e819889
IvanGSilva <ivanigs10@gmail.com>
2024-11-07 23:53:33 -0300

    -mod/add: modificada a model de usuário para receber email, criada a rota de login

--- servidor/routes/users.js
+++ servidor/routes/users.js
@@ -4,10 +4,29 @@ const router = express.Router();
 
 // Endpoint para criar um novo usuário
 router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  const user = new User({ username, password });
-  await user.save();
-  res.status(201).send(user);
+    try{
+        const { username, email, password } = req.body;
+        const user = new User({ username, email, password });
+        await user.save();
+        res.status(201).send(user);
+    }catch (error){
+        res.status(400).json({ error: 'Erro ao cadastrar usuário' });
+    }
 });
+
+// Endpoint para login
+router.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (user && await bcrypt.compare(password, user.password)) {
+        res.status(200).json({ message: 'Login realizado com sucesso!' });
+      } else {
+        res.status(401).json({ error: 'Credenciais inválidas' });
+      }
+    } catch (error) {
+      res.status(400).json({ error: 'Erro ao fazer login' });
+    }
+  });
 
 module.exports = router;
