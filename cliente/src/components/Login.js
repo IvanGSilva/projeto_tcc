027fb073eb287386307a44efadecf93a3f37c665
IvanGSilva <ivanigs10@gmail.com>
2024-11-25 00:04:12 -0300

    -mod: modificado o componente de login para poder ir para o formulario de adastro

--- cliente/src/components/Login.js
+++ cliente/src/components/Login.js
@@ -1,43 +1,47 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Login = ({ onLogin }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const Login = ({ onLogin, onRegister }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:5000/api/users/login', 
-                { email, password }, 
-                { withCredentials: true }
-            );
-            if (response.status === 200) {
-                onLogin(true); // Login bem-sucedido
-            }
-        } catch (error) {
-            console.error('Erro ao fazer login:', error);
-            onLogin(false); // Falha no login
-        }
-    };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/api/users/login',
+        { email, password },
+        { withCredentials: true }
+      );
+      if (response.status === 200) {
+        onLogin(true); // Login bem-sucedido
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      onLogin(false); // Falha no login
+    }
+  };
 
-    return (
-        <form onSubmit={handleLogin}>
-            <input 
-                type="email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-                placeholder="Email" 
-            />
-            <input 
-                type="password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
-                placeholder="Senha" 
-            />
-            <button type="submit">Entrar</button>
-        </form>
-    );
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+        />
+        <button type="submit">Entrar</button>
+      </form>
+      <button onClick={onRegister}>Criar Conta</button>
+    </div>
+  );
 };
 
 export default Login;
