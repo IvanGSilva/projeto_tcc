59c37e6f5daed848dfbac0359b5b73785bc848a8
IvanGSilva <ivanigs10@gmail.com>
2024-11-25 00:05:23 -0300

    -mod: modificado o componente app para lidar com o cadastro de usuário

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Login from './components/Login';
+import Register from './components/Register';
 import UserProfile from './components/UserProfile'; // Novo componente para perfil
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
 
   const handleLogin = (success) => {
     setIsAuthenticated(success);
@@ -17,7 +19,11 @@ const App = () => {
     <div>
       <h1>Clone do BlaBlaCar</h1>
       {!isAuthenticated ? (
-        <Login onLogin={handleLogin} />
+        showRegister ? (
+          <Register onBack={() => setShowRegister(false)} />
+        ) : (
+          <Login onLogin={handleLogin} onRegister={() => setShowRegister(true)} />
+        )
       ) : (
         <UserProfile onLogout={handleLogout} />
       )}
