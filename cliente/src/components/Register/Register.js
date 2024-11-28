61e77fa57780c957e0d9c65950199a0ecb966b54
IvanGSilva <ivanigs10@gmail.com>
2024-11-27 23:55:59 -0300

    -mod: correção do bug que completava os campos do cadastro de usuário com as credenciais salvas no navegador

--- cliente/src/components/Register/Register.js
+++ cliente/src/components/Register/Register.js
@@ -16,10 +16,10 @@ const Register = ({ onBack }) => {
       });
       if (response.ok) {
         alert('Cadastro realizado com sucesso!');
-        onBack(); // Voltar ao login
+        onBack();
       } else {
         const errorData = await response.json();
-      alert(`Erro no cadastro: ${errorData.message || 'Erro desconhecido'}`);
+        alert(`Erro no cadastro: ${errorData.message || 'Erro desconhecido'}`);
       }
     } catch (error) {
       console.error('Erro ao cadastrar:', error);
@@ -29,26 +29,38 @@ const Register = ({ onBack }) => {
   return (
     <div className={styles.container}>
       <h2>Cadastro</h2>
-      <form onSubmit={handleRegister}>
-        <input className={styles.input}
+      <form 
+        onSubmit={handleRegister} 
+        autoComplete="off"
+      >
+        <input
+          className={styles.input}
           type="text"
           placeholder="Nome de Usuário"
+          name="username"
           value={username}
           onChange={(e) => setName(e.target.value)}
+          autoComplete="new-username"
           required
         />
-        <input className={styles.input}
+        <input
+          className={styles.input}
           type="email"
           placeholder="Email"
+          name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          autoComplete="new-email"
           required
         />
-        <input className={styles.input}
+        <input
+          className={styles.input}
           type="password"
           placeholder="Senha"
+          name="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          autoComplete="new-password"
           required
         />
         <button type="submit" className={styles.button}>Cadastrar</button>
