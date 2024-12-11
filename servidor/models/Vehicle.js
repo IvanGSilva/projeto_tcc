c9ffce7d88c2d99fcff41412f55ee4c8fd205c19
IvanGSilva <ivanigs10@gmail.com>
2024-12-11 02:04:07 -0300

    -add: adicionada a model para veiculos

---
+++ servidor/models/Vehicle.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: String, required: true },
+  plate: { type: String, required: true, unique: true }
+}, { timestamps: true });
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+module.exports = Vehicle;
