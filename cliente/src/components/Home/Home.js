a4b144345bf86cc39080a50c64aea561ea9c6841
IvanGSilva <ivanigs10@gmail.com>
2025-01-04 01:26:20 -0300

    -mod: alteração no componente para adicionar botões de reserva e cancelamento de reserva

--- cliente/src/components/Home/Home.js
+++ cliente/src/components/Home/Home.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { searchRides } from '../../services/api'; // Função para buscar as viagens
+import React, { useState, useEffect } from 'react';
+import { searchRides, reserveRide, cancelReservation } from '../../services/api';
 import styles from './Home.module.css';
 
-const Home = () => {
+const Home = ({ loggedUserId }) => {
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
     const [rides, setRides] = useState([]);
@@ -37,62 +37,88 @@ const Home = () => {
         setDestination('');
         setRides([]);
         setSearchMade(false);
-        setError(null); // Limpa o erro
+        setError(null);
+    };
+
+    const handleReserve = async (rideId) => {
+        try {
+            const updatedRide = await reserveRide(rideId);
+            alert('Reserva realizada com sucesso!');
+            setRides((prevRides) =>
+                prevRides.map((ride) => (ride._id === updatedRide.ride._id ? updatedRide.ride : ride))
+            );
+        } catch (error) {
+            alert(`Erro ao reservar carona: ${error}`);
+        }
+    };
+
+    const handleCancelReservation = async (rideId) => {
+        try {
+            const updatedRide = await cancelReservation(rideId);
+            alert('Reserva cancelada com sucesso!');
+            setRides((prevRides) =>
+                prevRides.map((ride) => (ride._id === updatedRide.ride._id ? updatedRide.ride : ride))
+            );
+        } catch (error) {
+            alert(`Erro ao cancelar reserva: ${error}`);
+        }
     };
 
     return (
-        <>
-            <div className={styles.content}>
-                <div className={styles.container}>
-                    <div className={styles.rideSearchForm}>
-                        <h2 className={styles.title}>
-                            Encontre Caronas
-                        </h2>
-                        <div className={styles.inputDiv}>
-                            <i class="fa-solid fa-map-location-dot"></i>
-                            <input className={styles.input}
-                                type="text"
-                                placeholder="Onde você está agora?"
-                                value={origin}
-                                onChange={(e) => setOrigin(e.target.value)} />
-                        </div>
-
-                        <div className={styles.inputDiv}>
-                            <i class="fa-solid fa-route"></i>
-                            <input className={styles.input}
-                                type="text"
-                                placeholder="Para onde você vai?"
-                                value={destination}
-                                onChange={(e) => setDestination(e.target.value)} />
-                        </div>
-
-                        <div className={styles.buttonDiv}>
-                            <button className={styles.button} onClick={handleSearch} disabled={loading}>
-                                {loading ? 'Carregando...' : 'Buscar'}
-                            </button>
-
-                            <button className={styles.button} onClick={handleClearSearch}>
-                                Limpar
-                            </button>
-                        </div>
+        <div className={styles.content}>
+            <div className={styles.container}>
+                <div className={styles.rideSearchForm}>
+                    <h2 className={styles.title}>Encontre Caronas</h2>
+                    <div className={styles.inputDiv}>
+                        <i className="fa-solid fa-map-location-dot"></i>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            placeholder="Onde você está agora?"
+                            value={origin}
+                            onChange={(e) => setOrigin(e.target.value)}
+                        />
                     </div>
 
-                    {searchMade && rides.length > 0 && (
-                        <div>
-                            <h3>Resultados:</h3>
-                            <ul className={styles.list}>
-                                {rides.map((ride) => {
-                                    const rideDate = new Date(ride.date);
-                                    const formattedDate = rideDate.toLocaleString('pt-BR', {
-                                        day: '2-digit',
-                                        month: '2-digit',
-                                        hour: '2-digit',
-                                        minute: '2-digit',
-                                        hour12: false
-                                    });
+                    <div className={styles.inputDiv}>
+                        <i className="fa-solid fa-route"></i>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            placeholder="Para onde você vai?"
+                            value={destination}
+                            onChange={(e) => setDestination(e.target.value)}
+                        />
+                    </div>
 
-                                    return (
-                                        <li className={styles.listItem} key={ride._id}>
+                    <div className={styles.buttonDiv}>
+                        <button className={styles.button} onClick={handleSearch} disabled={loading}>
+                            {loading ? 'Carregando...' : 'Buscar'}
+                        </button>
+
+                        <button className={styles.button} onClick={handleClearSearch}>
+                            Limpar
+                        </button>
+                    </div>
+                </div>
+
+                {searchMade && rides.length > 0 && (
+                    <div>
+                        <h3>Resultados:</h3>
+                        <ul className={styles.list}>
+                            {rides.map((ride) => {
+                                const rideDate = new Date(ride.date);
+                                const formattedDate = rideDate.toLocaleString('pt-BR', {
+                                    day: '2-digit',
+                                    month: '2-digit',
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                    hour12: false
+                                });
+
+                                return (
+                                    <li className={styles.listItem} key={ride._id}>
+                                        <div className={styles.ride_result}>
                                             <div className={styles.ride_data}>
                                                 <div><strong>Origem:</strong> {ride.origin}</div>
                                                 <div><strong>Destino:</strong> {ride.destination}</div>
@@ -100,7 +126,7 @@ const Home = () => {
                                                 <div><strong>Assentos:</strong> {ride.seats}</div>
                                             </div>
 
-                                            <div className={styles.driver_and_vahicle}>
+                                            <div className={styles.driver_and_vehicle}>
                                                 {ride.driver && (
                                                     <>
                                                         <div className={styles.imageContainer}>
@@ -108,7 +134,8 @@ const Home = () => {
                                                                 <img
                                                                     className={styles.profileImage}
                                                                     src={`http://localhost:5000/${ride.driver.profilePicture}`}
-                                                                    alt={`Foto de perfil de ${ride.driver.username}`} />
+                                                                    alt={`Foto de perfil de ${ride.driver.username}`}
+                                                                />
                                                             )}
                                                         </div>
                                                         <div><strong>Condutor:</strong> {ride.driver.username}</div>
@@ -123,23 +150,41 @@ const Home = () => {
                                                     </div>
                                                 )}
                                             </div>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                    )}
+                                        </div>
 
-                    {searchMade && rides.length === 0 && !loading && !error && (
-                        <p className={styles.noResults}>Nenhuma carona encontrada.</p>
-                    )}
-                </div>
+                                        <div className={styles.ride_options}>
+                                            {ride.passengers.includes(loggedUserId) ? (
+                                                <button
+                                                    className={styles.button}
+                                                    onClick={() => handleCancelReservation(ride._id)}
+                                                >
+                                                    Cancelar Reserva
+                                                </button>
+                                            ) : (
+                                                <button
+                                                    className={styles.button}
+                                                    onClick={() => handleReserve(ride._id)}
+                                                >
+                                                    Reservar
+                                                </button>
+                                            )}
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                )}
 
-                <div className={styles.map}>
-                    {/* mapa placeholder */}
-                </div>
+                {searchMade && rides.length === 0 && !loading && !error && (
+                    <p className={styles.noResults}>Nenhuma carona encontrada.</p>
+                )}
             </div>
-        </>
+
+            <div className={styles.map}>
+                {/* Mapa placeholder */}
+            </div>
+        </div>
     );
 };
 
